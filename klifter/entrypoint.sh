78892ad1d1c3cb32dda270c847c336887fcca01b
David Delassus <david.jose.delassus@gmail.com>
2022-01-22 04:41:34 +0100

    :whale: :bug: pass input as argument to entrypoint #klifter

--- klifter/entrypoint.sh
+++ klifter/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 export K8S_STATE_USER="$(whoami)"
 export K8S_STATE_SOURCE_KIND="local"
-export K8S_STATE_SOURCE_LOCAL_DIR="${INPUT_PATH}"
+export K8S_STATE_SOURCE_LOCAL_DIR="$1"
 
 cd /workspace
 poetry run ansible-playbook site.yml
