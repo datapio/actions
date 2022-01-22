94ede1362197cce1c802866e454824d5fa84ac00
David Delassus <david.jose.delassus@gmail.com>
2022-01-22 06:07:12 +0100

    :loud_sound: add debugging message #klifter

--- klifter/entrypoint.sh
+++ klifter/entrypoint.sh
@@ -6,5 +6,9 @@ export K8S_STATE_USER="$(whoami)"
 export K8S_STATE_SOURCE_KIND="local"
 export K8S_STATE_SOURCE_LOCAL_DIR="$1"
 
+echo "::debug::K8S_STATE_USER=${K8S_STATE_USER}"
+echo "::debug::K8S_STATE_SOURCE_KIND=${K8S_STATE_SOURCE_KIND}"
+echo "::debug::K8S_STATE_SOURCE_LOCAL_DIR=${K8S_STATE_SOURCE_LOCAL_DIR}"
+
 cd /workspace
 poetry run ansible-playbook site.yml
